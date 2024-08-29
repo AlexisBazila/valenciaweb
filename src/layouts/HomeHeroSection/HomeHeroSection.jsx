f5774135eb912df61de9b71d7f238d411e3e1e53
AlexisBazila <alexisbazila@proton.me>
2024-08-29 16:47:04 -0300

    HomeHeroSection primeros estilos

---
+++ src/layouts/HomeHeroSection/HomeHeroSection.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./HomeHeroSection.css"; // Importa los estilos CSS para la sección
+import NavVal from "../../components/NavVal/NavVal";
+import NonImage from "../../assets/images/nonimage.jpg";
+
+const HomeHeroSection = () => {
+  return (
+    <>
+      <NavVal />
+      <section className="hero-section">
+        <div className="container">
+          <div className="hero-content">
+            <h2 className="hero-title">Cuidando tu belleza y bienestar</h2>
+            <h1 className="hero-subtitle">INSTITUTO VALENCIA</h1>
+            <p className="hero-description">
+              Descubre un oasis de belleza, salud y bienestar. Ofrecemos
+              servicios de estética, cosmética, podología, gimnasio, y
+              consultorio nutricional. Además, contamos con cursos y productos
+              exclusivos para tu cuidado personal.
+            </p>
+            <button className="hero-button">CONÓCENOS</button>
+          </div>
+          <div className="hero-image">
+            {/* <img src={NonImage} alt="Instituto Valencia" /> */}
+            <div className="ImgTest"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomeHeroSection;
