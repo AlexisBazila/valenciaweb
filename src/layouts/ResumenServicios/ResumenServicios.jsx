74c5b86643b4657651ccf982201cb790214449d4
AlexisBazila <alexisbazila@proton.me>
2024-09-25 14:47:21 -0300

    iconos

--- src/layouts/ResumenServicios/ResumenServicios.jsx
+++ src/layouts/ResumenServicios/ResumenServicios.jsx
@@ -7,8 +7,10 @@ import { GiFootprint } from "react-icons/gi";
 import { PiHairDryerFill } from "react-icons/pi";
 import { TbMassage } from "react-icons/tb";
 import { FaSpa } from "react-icons/fa";
+import { FaSprayCanSparkles } from "react-icons/fa6";
 
 import Makeup from "../../assets/images/Makeup.svg";
+import Depilacion from "../../assets/images/depilacion.svg";
 
 const ResumenServicios = () => {
   return (
@@ -22,10 +24,13 @@ const ResumenServicios = () => {
       <div className="ContenedorServicios">
         <IconServices name="PODOLOGIA" icon={<GiFootprint />} />
         <IconServices name="PELUQUERIA" icon={<PiHairDryerFill />} />
-        <IconServices name="MAQUILLAJE" />
+        <IconServices name="MAQUILLAJE" icon={<img src={Makeup} alt="" />} />
         <IconServices name="MASAJES" icon={<TbMassage />} />
-        <IconServices name="DEPILACIÓN" />
-        <IconServices name="COSMETOLOGIA" />
+        <IconServices
+          name="DEPILACIÓN"
+          icon={<img src={Depilacion} alt="" />}
+        />
+        <IconServices name="COSMETOLOGIA" icon={<FaSprayCanSparkles />} />
         <IconServices name="APARATOLOGIA" />
         <IconServices name="SPA" icon={<FaSpa />} />
         <IconServices name="DERMAPEN" />
