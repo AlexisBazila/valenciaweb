a41c5cba172f5d6c8ddb392856b397dfa527b31b
AlexisBazila <alexisbazila@proton.me>
2024-09-27 16:02:12 -0300

    footer

--- src/layouts/FooterVal/FooterVal.jsx
+++ src/layouts/FooterVal/FooterVal.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./FooterVal.css";
+import { CiFacebook, CiInstagram } from "react-icons/ci";
 import logoval from "../../assets/images/logoval.jpg";
+import socialmedia from "../../assets/images/socialmedia.svg";
 
 function FooterVal() {
   return (
@@ -9,6 +11,16 @@ function FooterVal() {
         <div className="LogoFooter">
           <div className="ImgLogoFooter">
             <img src={logoval} alt="" />
+          </div>
+        </div>
+        <div className="RedesFooter">
+          <div className="IconoRedesFooter">
+            <img src={socialmedia} alt="" />
+            <p>SIGUENOS</p>
+          </div>
+          <div className="LinkRedesFooter">
+          <i><CiFacebook /></i><a href="">Facebook</a>
+            <i><CiInstagram /></i><a href="">Instagram</a>
           </div>
         </div>
       </div>
