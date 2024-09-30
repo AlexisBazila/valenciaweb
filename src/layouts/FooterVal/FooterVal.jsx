a7f4a7a844752bec4aaa7678948d2c42cc593a63
AlexisBazila <alexisbazila@proton.me>
2024-09-30 14:33:10 -0300

    Footer OK

--- src/layouts/FooterVal/FooterVal.jsx
+++ src/layouts/FooterVal/FooterVal.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./FooterVal.css";
 import { CiFacebook, CiInstagram } from "react-icons/ci";
+import { FaWhatsapp } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
+import { SiGooglemaps } from "react-icons/si";
 import logoval from "../../assets/images/logoval.jpg";
 import socialmedia from "../../assets/images/socialmedia.svg";
+import contacto from "../../assets/images/contacto.svg";
 
 function FooterVal() {
   return (
@@ -19,8 +23,52 @@ function FooterVal() {
             <p>SIGUENOS</p>
           </div>
           <div className="LinkRedesFooter">
-          <i><CiFacebook /></i><a href="">Facebook</a>
-            <i><CiInstagram /></i><a href="">Instagram</a>
+            <a href="" className="LinkRedes">
+              <i>
+                <CiFacebook />
+              </i>
+              FACEBOOK
+            </a>
+            <a href="" className="LinkRedes">
+              {" "}
+              <i>
+                <CiInstagram />
+              </i>
+              INSTAGRAM
+            </a>
+          </div>
+        </div>
+        <div className="RedesFooter">
+          <div className="IconoContactoFooter">
+            <img src={contacto} alt="" />
+            <p>CONTACTANOS</p>
+          </div>
+          <div className="LinkRedesFooter">
+            <a href="" className="LinkContacto">
+              <i>
+                <FaWhatsapp />
+              </i>
+              +54 3755 1500000
+            </a>
+            <a href="" className="LinkContacto">
+              {" "}
+              <i>
+                <MdAlternateEmail />
+              </i>
+              miemail@valencia.com.ar
+            </a>
+            <a href="" className="LinkContacto">
+              {" "}
+              <i>
+                <SiGooglemaps />
+              </i>
+              Av. Italia ####
+            </a>
+          </div>
+        </div>
+        <div className="LogoFooter">
+          <div className="ImgLogoFooter">
+            <img src={logoval} alt="" />
           </div>
         </div>
       </div>
