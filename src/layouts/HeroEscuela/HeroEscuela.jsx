a2c1eebdeaac226f1b526b7c86b47f6a5a0d471c
AlexisBazila <alexisbazila@proton.me>
2024-11-25 13:57:14 -0300

    HeroEscuela ok

--- src/layouts/HeroEscuela/HeroEscuela.jsx
+++ src/layouts/HeroEscuela/HeroEscuela.jsx
@@ -4,13 +4,12 @@ import TituloPrincipal from "../../components/TituloPrincipal/TituloPrincipal";
 
 function HeroEscuela() {
   return (
-    <section>
-      <div>
-        <TituloPrincipal
-          titulo="Escuela"
-          subTitulo="Transforma tu pasión en profesión con nuestros cursos especializados. ¡Tu futuro comienza aquí!"
-        />
-      </div>
+    <section className="HeroEscuela">
+      <div className="EscuelaHeroPick"></div>
+      <TituloPrincipal
+        titulo="Escuela"
+        subTitulo="Transforma tu pasión en profesión con nuestros cursos especializados. ¡Tu futuro comienza aquí!"
+      />
     </section>
   );
 }
