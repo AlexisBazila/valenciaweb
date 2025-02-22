6bec9bccbbd833f8c9e22357f073c0a8cdfac5e1
Alexis A. Bazila <alexisbazila@proton.me>
2025-02-22 07:59:06 -0300

    css contacto eliminado test

--- src/layouts/Contacto/Contacto.jsx
+++ src/layouts/Contacto/Contacto.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import "./Contacto.css";
-import { FaFacebookF, FaInstagram, FaWhatsapp, FaMapMarkerAlt } from "react-icons/fa";
+
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaWhatsapp,
+  FaMapMarkerAlt,
+} from "react-icons/fa";
 
 function Contacto() {
   const whatsappNumber = "+5493755232012";
-  const googleMapsSrc = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.06132112047!2d-55.12053622544557!3d-27.498468317930936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f8f54f3702b837%3A0x1be24917bf3999db!2sInstituto%20Valencia!5e0!3m2!1ses!2sar!4v1740098274207!5m2!1ses!2sar"; // Reemplaza con el link correcto de Google Maps
+  const googleMapsSrc =
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.06132112047!2d-55.12053622544557!3d-27.498468317930936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f8f54f3702b837%3A0x1be24917bf3999db!2sInstituto%20Valencia!5e0!3m2!1ses!2sar!4v1740098274207!5m2!1ses!2sar"; // Reemplaza con el link correcto de Google Maps
 
   return (
     <div className="contact-page">
       <div className="contact-header">
         <h1>Contáctanos</h1>
-        <p>Estamos aquí para ayudarte. Escríbenos o visítanos en nuestras redes.</p>
+        <p>
+          Estamos aquí para ayudarte. Escríbenos o visítanos en nuestras redes.
+        </p>
       </div>
 
       <div className="contact-content">
@@ -29,16 +37,23 @@ function Contacto() {
         <div className="contact-social">
           <h2>Síguenos en redes sociales</h2>
           <div className="social-icons">
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaFacebookF />
             </a>
-            <a href="https://www.instagram.com/instituto.valencia/?hl=es" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.instagram.com/instituto.valencia/?hl=es"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaInstagram />
             </a>
           </div>
         </div>
 
-    
         <div className="contact-map">
           <h2>Visítanos</h2>
           <p>
--- src/pages/Escuela.jsx
+++ src/pages/Escuela.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroEscuela from "../layouts/HeroEscuela/HeroEscuela";
+import Certificate from "../layouts/Certificate/Certificate";
 
 function Escuela() {
   return (
     <>
       <HeroEscuela />
+      <Certificate />
     </>
   );
 }
