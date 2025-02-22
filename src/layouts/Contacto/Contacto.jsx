23e0826d2a1b07e0fadfb951add288e03d37d61d
Alexis A. Bazila <alexisbazila@proton.me>
2025-02-22 08:03:05 -0300

    css contacto restaurado

--- src/layouts/Contacto/Contacto.jsx
+++ src/layouts/Contacto/Contacto.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Contacto.css";
 
 import {
   FaFacebookF,
@@ -10,7 +11,7 @@ import {
 function Contacto() {
   const whatsappNumber = "+5493755232012";
   const googleMapsSrc =
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.06132112047!2d-55.12053622544557!3d-27.498468317930936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f8f54f3702b837%3A0x1be24917bf3999db!2sInstituto%20Valencia!5e0!3m2!1ses!2sar!4v1740098274207!5m2!1ses!2sar"; // Reemplaza con el link correcto de Google Maps
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3539.06132112047!2d-55.12053622544557!3d-27.498468317930936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f8f54f3702b837%3A0x1be24917bf3999db!2sInstituto%20Valencia!5e0!3m2!1ses!2sar!4v1740098274207!5m2!1ses!2sar";
 
   return (
     <div className="contact-page">
